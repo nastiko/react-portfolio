5346705bdade389173f63bc1c19697636ae9d181
Anastasia H. <anastasia.hrynkevich@outlook.com>
2023-12-01 17:24:45 +0000

    Working on portfolio: - added imgs folder in public folder, all imgs have appeared

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,16 @@ export default function Footer() {
     return (
         <div className="h-28 bg-[#161619] flex items-center justify-center gap-4 mb-6">
             <a href="https://twitter.com/">
-                <img src="../imgs/icons/twitter.svg" />
+                <img src="../imgs/icons/twitter.svg" alt="Twitter"/>
             </a>
             <a href="https://www.facebook.com/">
-                <img src="../imgs/icons/facebook.svg" />
+                <img src="../imgs/icons/facebook.svg" alt="Facebook"/>
             </a>
             <a href="https://www.instagram.com/">
-                <img src="../imgs/icons/instagram.svg" />
+                <img src="../imgs/icons/instagram.svg" alt="Instagram"/>
             </a>
             <a href="https://github.com/nastiko">
-                <img src="../imgs/icons/gitHub.svg" />
+                <img src="../imgs/icons/gitHub.svg" alt="GitHub"/>
             </a>
         </div>
     )
