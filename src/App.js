1784d9ed616b605654e0d57f77902226f629dcd0
Anastasia H. <anastasia.hrynkevich@outlook.com>
2023-12-04 16:16:03 +0000

    Working on portfolio: - added header content, still working on "main" content - adding styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react"
+import Header from "./components/Header"
+import Main from "./components/Main"
 import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div>
-      <Footer />
+    <div className="w-full bg-[#1a1b21]">
+        <div className="w-[500px] py-12 mx-auto">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,28 @@
+export default function Main() {
+    return (
+        <div>
+            <div className="flex flex-col items-center justify-center
+                            prose prose-h1:text-[#ffffff] prose-h1:text-[32px] prose-h1:leading-[30px] prose-h1:font-bold prose-h1:my-[21px]
+                            prose-h3:text-[#f3bf99] prose-h3:text-[24px] prose-h3:font-normal prose-h3:leading-[15px] prose-h3:my-0
+                            prose-a:text-[#f5f5f5] prose-a:text-[16px] prose-a:font-normal prose-a:no-underline prose-a:my-[16px]">
+                <h1>Anastasia Hrynkevich</h1>
+                <h3>Frontend Developer</h3>
+                <a href="https://anastasia.grinkevi.ch/">anastasia.grinkevi.ch</a>
+            </div>
+            <div>
+                <a href="">Email</a>
+                <a href="">Linkedln</a>
+            </div>
+            <div>
+                <div>
+                    <h2>About</h2>
+                    <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+                </div>
+                <div>
+                    <h2>Interests</h2>
+                    <p>Sport: tennis, swimming, pilates. Music scholar. Reader. Meditation. Adore nature. Hot chocolate fanatic.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
