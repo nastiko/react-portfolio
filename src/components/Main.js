bb5d39b7c7a23cc93fc1c0e4698df62699681e7e
Anastasia H. <anastasia.hrynkevich@outlook.com>
2023-12-05 17:24:55 +0000

    Working on portfolio: - added styles for Main component, still need a bit fix it

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,11 +9,23 @@ export default function Main() {
                 <h3>Frontend Developer</h3>
                 <a href="https://anastasia.grinkevi.ch/">anastasia.grinkevi.ch</a>
             </div>
-            <div>
-                <a href="">Email</a>
-                <a href="">Linkedln</a>
+            <div className="flex justify-around mt-8
+                            prose prose-a:no-underline
+                            prose-p:text-[20px] prose-p:leading-[16px] prose-p:my-0">
+                <a className="flex items-center gap-3.5 border-[1px] border-[#d1d5db] bg-[#ffffff] rounded-lg py-3.5 px-10"
+                   href="https://anastasia.grinkevi.ch/#section-contact">
+                    <img className="w-[30px] h-[30px] my-0" src="../imgs/icons/email.svg" alt="Icon email"/>
+                    <p>Email</p>
+                </a>
+                <a className="flex items-center gap-3.5 border-[1px] border-[#5093e2] bg-[#5093e2] rounded-lg py-3.5 px-10"
+                   href="https://www.linkedin.com/in/anastasia-grinkevich?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium">
+                    <img className="w-[30px] h-[30px] my-0" src="../imgs/icons/linkedin.svg" alt="Icon linkedIn"/>
+                    <p className="text-[#ffffff]">LinkedIn</p>
+                </a>
             </div>
-            <div>
+            <div className="py-12 px-10
+                            prose prose-h2:text-[30px] prose-h2:font-medium prose-h2:text-[#f5f5f5] prose-h2:my-0
+                            prose-p:text-[16px] prose-p:text-[#dcdcdc] prose-p:font-thin prose-p:break-words proe-p:my-0">
                 <div>
                     <h2>About</h2>
                     <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
